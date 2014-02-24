78c992476f393a7521cb06d416def3fed305988d
Andreas Graßl <andreasgrassl@gmx.de>
2014-02-24 23:23:41 +0100

    cleaning up code
    cleaned up the few lines and added some output to indicate it's working

--- savingFiles.py
+++ savingFiles.py
@@ -1,10 +1,19 @@
 import os
 import shutil
+import time
 
 src = "C:\\Users\\Andreas Graßl\\Desktop\\Programme\\ATLauncher\\"
 dest = "F:\\minecraft\\ATLauncher\\"
+def saving():
+    start = time.time()
+    if os.path.isdir(dest):
+        print("deleting old folder..")
+        shutil.rmtree(dest)
+        print("deleting successful")
+    print("copying actual folder..")
+    shutil.copytree(src, dest, symlinks=False, ignore=None)
+    end = time.time()
+    print ("copying successful")
+    print ("time elapsed was: " + str(end-start))
 
-if os.path.isdir(dest):
-    shutil.rmtree(dest);
-
-shutil.copytree(src, dest, symlinks=False, ignore=None)
+saving()
